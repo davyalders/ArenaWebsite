15e441f4e5870a3663036f90b3b3ee72a3190a60
davyalders <davyalders@gmail.com>
2016-01-26 17:41:06 +0100

    download

--- Arena/Default.aspx
+++ Arena/Default.aspx
@@ -3,4 +3,9 @@
     <link href="Main.css" rel="stylesheet" type="text/css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id ="reg1">
+         <h1>Welkom bij Arena of legends</h1>
+        <asp:Button ID="Button1" runat="server" Text="Download" OnClick="Button1_Click" />
+    </div>
+   
 </asp:Content>
--- Arena/Default.aspx.cs
+++ Arena/Default.aspx.cs
@@ -13,5 +13,13 @@ namespace Arena
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "application/octect-stream";
+            Response.AppendHeader("content-disposition","filename=Abby.jpg");
+            Response.TransmitFile(Server.MapPath("~/Files/Abby.jpg"));
+            Response.End();
+        }
     }
 }
